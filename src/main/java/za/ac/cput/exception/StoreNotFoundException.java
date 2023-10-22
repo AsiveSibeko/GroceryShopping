56c91206de61e71982eccbea413d37ecba0e91d1
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2023-10-22 23:15:18 +0200

    Exception folder deleted.

--- src/main/java/za/ac/cput/exception/StoreNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package za.ac.cput.exception;
-
-public class StoreNotFoundException extends RuntimeException{
-    public StoreNotFoundException(Long id){
-        super("Could not find the store wih id " +id);
-    }
-}
